7ebfe188c2a304302656a5e6b8ce2155ea3cf3f5
jamesgreen-c <jamesenricodavidgreen@gmail.com>
2026-03-10 15:51:42 +0000

    Add method to use dynamic resampling in these models

--- gradient_csmc/utils/resamplings.py
+++ gradient_csmc/utils/resamplings.py
@@ -2,11 +2,60 @@
 Implementation of conditional multinomial and killing resampling algorithms.
 """
 from functools import partial
+from typing import Callable
 
 import jax
 import jax.numpy as jnp
 from chex import Array, PRNGKey
 from jax.scipy.special import logsumexp
+
+
+def dynamic(resampling_fn: Callable, threshold: float, key: PRNGKey, weights: Array, i: int, j: int, conditional: bool = True) -> Array:
+    """
+    Dynamic resampling. The weights are assumed to be normalised already.
+    This function should be closed over with a predefined resampling function and threshold: 
+
+        e.g. 
+        threshold = 0.5
+        resampling_fn = multinomial
+        def resampling_fn(key, weights, i, j, conditional):
+            return dynamic(resampling_fn, threshold, key, weights, i, j, conditional)
+    
+    Parameters
+    ----------
+    key:
+        Random number generator key.
+    weights:
+        Weights of the particles.
+    i, j:
+        Conditional indices: the resampling is conditioned on the fact that the ancestor at index j is equal to i.
+    conditional:
+        If True, the resampling is conditional on the fact that the ancestor at index j is equal to i.
+        Otherwise, it's the standard resampling
+    threshold:
+        Is a fraction defining the threshold ESS (against the number of particles) that must be met before resampling.
+        
+    Returns
+    -------
+    indices:
+        Indices of the resampled particles.
+    """
+    N = weights.shape[0]
+    # jax.debug.print("resampling weights shape: {}", weights.shape)    # output: (N,)
+    _ess = 1.0 / jnp.sum(weights**2)
+
+    def resample(_):
+        return resampling_fn(key, weights, i, j, conditional)
+
+    def skip(_):
+        return jnp.arange(N)
+
+    return jax.lax.cond(
+        _ess < N * threshold,
+        resample,
+        skip,
+        operand=None,
+    )
 
 
 def multinomial(key: PRNGKey, weights: Array, i: int, j: int, conditional: bool = True) -> Array:
